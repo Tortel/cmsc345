33be815f5a5fc0f8e23e94c0e2c49d616c41e642
Scott Warner <Tortel1210@gmail.com>
2011-10-18 04:54:51 +0000

    Changed how data is loaded at start

--- app/repo/Bootstrap.java
+++ app/repo/Bootstrap.java
@@ -9,10 +9,20 @@ import models.*;
 @OnApplicationStart
 public class Bootstrap extends Job {
  
-    public void doJob() {
+    @SuppressWarnings("unused")
+	public void doJob() {
         // Check if the database is empty
         if(User.count() == 0) {
-            Fixtures.loadModels("data.yml");
+        	//The really handy YML file doesnt handle inheritance well
+            //Fixtures.loadModels("data.yml");
+        	Physician bob = new Physician("bob@gmail.com", "secret", "Bob", "Doctor").save();
+        	Physician george = new Physician("george@gmail.com", "secret", "George", "Test").save();
+        	Patient bill = new Patient("bill@gmail.com", "secret", "Bill", "Sick", "123 Main Street", "410-000-0000", 'M').save();
+        	Patient harriet = new Patient("harriet@gmail.com","secret", "Harriet", "Blarg", "124 Main Street", "410-000-0001", 'F').save();
+        	
+        	Exam exam1 = new Exam(bill, george, "Bill Sucks!", "Bob is awesome!").save();
+        	Exam exam2 = new Exam(harriet, bob, "Harriet is awesome!", "Bob is awesome!").save();
+        	
         }
     }
  
