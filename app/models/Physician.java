c0684a792bb4ae7ab52d5234f8776fd4e4f709cf
Scott Warner <Tortel1210@gmail.com>
2011-10-18 14:18:38 +0000

    Testing if Inheritance breaks play

--- app/models/Physician.java
+++ app/models/Physician.java
@@ -11,10 +11,18 @@ import play.db.jpa.*;
  * 
  */
 @Entity
-public class Physician extends User {
+public class Physician extends Model {
 	//All the exams they have conducted
 	@OneToMany(mappedBy="physician", cascade=CascadeType.ALL)
 	private List<Exam> exams;
+	
+	
+	//From User
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	//End from User
 	
 	/**
 	 * Creates a new Physician
@@ -22,8 +30,14 @@ public class Physician extends User {
 	 * @param password the physician's password
 	 */
 	public Physician(String username, String password, String firstName, String lastName){
-		super(username, password, firstName, lastName);
+		//super(username, password, firstName, lastName);
 		exams = new ArrayList<Exam>();
+		//from User
+		this.username = username;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		//End from User
 	}
 	
 	/**
@@ -34,5 +48,27 @@ public class Physician extends User {
 	public List<Exam> getExams(){
 		return exams;
 	}
+	
+	//From User
+	/**
+	 * Sends the user their password over email
+	 */
+	public void recoverPassword(){
+		//TODO: Implement this
+	}
+	
+	public String getUsername(){
+		return username;
+	}
+	
+	public String getPassword(){
+		return password;
+	}
+	
+	public String getName(){
+		return firstName + " " + lastName;
+	}
+	
+	//End from User
 	
 }
