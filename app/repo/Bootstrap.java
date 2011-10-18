bbbb7bedd943ecbb5c086c450be4e07df1bcd02f
Scott Warner <Tortel1210@gmail.com>
2011-10-18 03:44:48 +0000

    Added data to load on start, and changed one package

---
+++ app/repo/Bootstrap.java
@@ -0,0 +1,19 @@
+package repo;
+
+import play.*;
+import play.jobs.*;
+import play.test.*;
+ 
+import models.*;
+ 
+@OnApplicationStart
+public class Bootstrap extends Job {
+ 
+    public void doJob() {
+        // Check if the database is empty
+        if(User.count() == 0) {
+            Fixtures.loadModels("data.yml");
+        }
+    }
+ 
+}
