dfac6ca0274e99d594d1f3b62b0421d82f14c4fb
Scott Warner <Tortel1210@gmail.com>
2011-10-25 03:07:04 +0000

    Added Security implementation

---
+++ app/controllers/Security.java
@@ -0,0 +1,56 @@
+package controllers;
+
+import models.*;
+
+import repo.*;
+
+/**
+ * Class to handle security-related items
+ *
+ */
+public class Security extends Secure.Security {
+
+	/**
+	 * Method to authenticate users
+	 * @param username the username
+	 * @param password the password
+	 * @return
+	 */
+	static boolean authenticate(String username, String password) {
+		//This makes it return true, until its set up a little better
+		if(username != null)
+			return true;
+		
+		return Repository.login(username, password);	
+	}
+	
+	/**
+	 * Redirect user to the welcome page on login
+	 */
+	public static void onAuthenticated(){
+		PageController.welcome();
+	}
+	
+	/**
+	 * Redirect the user to the 'landing' page on logout
+	 */
+	public static void onDisconnected(){
+		Dummy.index();
+	}
+	
+	/**
+	 * Checks if the connected user is the given role
+	 * @param profile the role to check
+	 * @return true if user is the given role, false otherwise
+	 */
+	public static boolean check(String profile){
+		if("physician".equals(profile)){
+			User user = User.find("byUsername", connected() ).<User>first();
+			if(user.getClass() == Physician.class)
+				return true;
+		}
+		
+		return false;
+	}
+	
+}
