75f3052b84c9328ce24b76b368d402e652467e7c
Scott Warner <Tortel1210@gmail.com>
2011-11-01 22:11:38 +0000

    More encryption being enabled

--- app/controllers/Dummy.java
+++ app/controllers/Dummy.java
@@ -60,6 +60,9 @@ public class Dummy extends Controller {
     		
     	}
     	
+    	//Encrypt the password
+    	password = Repository.encodePassword(password);
+    	
     	if (validation.hasErrors()) {
             render("Dummy/createAccount.html", email, firstName, lastName, address, phoneNumber, code);
         }
